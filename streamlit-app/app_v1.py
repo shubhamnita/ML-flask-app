f0448e33a7fb768a47427bb886520ccb3dd7af60
Shubham <shubhamkapoorece@gmail.com>
2019-10-18 00:43:49 +0530

    streamlit version 1 completed

--- streamlit-app/app_v1.py
+++ streamlit-app/app_v1.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
 
-st.title('Kaggle Titanic Dataset')
+st.title('Who survived Titanic Crash - Jake or Rose')
 
 # Load the dataset in a dataframe object and include only four features as mentioned
 
@@ -13,12 +14,65 @@ def main():
 	data = load_data(1000)
 	data_load_state.text('Loading data... done!')
 	if st.checkbox('Show raw data'):
-		st.subheader('Raw data')
+		st.subheader('Kaggle Titanic Dataset')
 		st.write(data)
-	st.subheader('Feature Columns Selection')
-	features = st.multiselect('Feature-Columns',data.columns)
-	st.write(features)
-	target = st.selectbox('Target Column',data.columns)
+	st.subheader('Feature and Target Columns Selection')
+	feature_columns = st.multiselect('Feature-Columns',data.columns)
+	st.write(feature_columns)
+	dependent_variable = st.selectbox('Target Column',data.columns)
+	include = feature_columns
+	include.append(dependent_variable)
+	df_ = data[include]
+	if st.checkbox('Show filtered data'):
+		st.subheader('Filtered data')
+		st.write(df_)
+		
+	# Data Preprocessing
+	st.subheader('Data Preprocessing and Cleaning Steps')
+	st.text('Null Values information')
+	st.write(df_.isnull().sum())
+	na_remove_state = st.text('Removing Null values...')
+	df_.dropna(axis=0,inplace=True)
+	na_remove_state.text('Removed Null values... done!')
+	if st.checkbox('Show filtered data after null values removal'):
+		st.subheader('Filtered data')
+		st.write(df_)
+
+	categoricals = []
+	for col, col_type in df_.dtypes.iteritems():
+		if col_type == 'O':
+			categoricals.append(col)
+		else:
+			df_[col].fillna(0, inplace=True)
+
+	df_ohe = pd.get_dummies(df_, columns=categoricals)
+	if st.checkbox('Show filtered data after handling categorical data'):
+		st.subheader('Filtered data')
+		st.write(df_ohe)
+
+	if st.checkbox('Train the classification model'):
+		algorithm = st.selectbox('Classification Algorithm',['LogisticRegression'])
+		dependent_variable = 'survived'
+		if dependent_variable in df_ohe.columns:
+			x = df_ohe[df_ohe.columns.difference([dependent_variable])]
+			y = df_ohe[dependent_variable]
+			model = train_model(x,y)
+
+	if st.checkbox('Want to predict'):
+		st.subheader('Enter Test data')
+		test_data ={}
+		for var in df_ohe.columns.difference([dependent_variable]):
+			test_data[var] = int(st.text_input(var))
+
+		#st.write(test_data)
+		#test_df = pd.DataFrame(test_data,columns=df_ohe.columns.difference([dependent_variable]))
+		st.subheader('Test data is: ')
+		test_df = pd.DataFrame([test_data])
+		st.write(test_df)
+		prediction = list(model.predict(test_df))
+		st.subheader('Prediction is: ')
+		st.write(str(prediction[0]))
+				
 
 
 @st.cache
@@ -27,6 +81,12 @@ def load_data(nrows):
 	lowercase = lambda x: str(x).lower()
 	data.rename(lowercase, axis='columns', inplace=True)
 	return data
+
+def train_model(x,y):
+	# Logistic Regression classifier
+	lr = LogisticRegression()
+	lr.fit(x, y)
+	return lr
 
 if __name__ == "__main__":
     main()
